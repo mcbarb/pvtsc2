254c8706bf795326e8259f6bdf6a02f927d769f2
Marcelo Barbosa <marcelo.barbosa@nubank.com.br>
2017-06-17 09:23:29 -0300

    uncomitted changes

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -8,25 +8,27 @@ final case class Minus(a: Expr, b: Expr) extends Expr
 final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
-object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+object Calculator extends App {
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions map { case (name, exprSignal) => (name, Signal(
+      if (true) eval(exprSignal(),namedExpressions) else Double.NaN
+    )) }
   }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
+    case Literal(v) => v
+    case Ref(name) => eval(getReferenceExpr(name,references),references.filterKeys(_ != name))
+    case Plus(a,b) => eval(a, references) + eval(b, references)
+    case Minus(a,b) => eval(a, references) - eval(b, references)
+    case Times(a,b) => eval(a, references) * eval(b, references)
+    case Divide(a,b) => eval(a, references) / eval(b, references)
+    case _ => Double.NaN
   }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
-  private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
-    references.get(name).fold[Expr] {
-      Literal(Double.NaN)
-    } { exprSignal =>
-      exprSignal()
-    }
+  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]): Expr = {
+    references.get(name).fold[Expr] {Literal(Double.NaN)} {exprSignal => exprSignal()}
   }
 }
--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,12 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal(b()*b() - 4*a()*c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    val d = computeDelta(a,b,c)
+    Signal(if (a() < 0) Set() else if (a() == 0) { Set( -b() / (2 * a()) ) } else Set( (-b() + d())/ (2 * a()) , (-b() - d())/ (2 * a())))
   }
 }
